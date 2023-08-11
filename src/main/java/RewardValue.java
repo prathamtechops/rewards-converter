9559cc66989f9b6437edbf77d13f8a22f3b5b0c2
Pratham-Techops <poddarpratham@gmail.com>
2023-08-11 17:20:00 +0530

    Commint

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,14 @@
+public class RewardValue {
+    private double cashValue;
+    public  RewardValue(double cashValue){
+        this.cashValue = cashValue;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public double getMilesValue(){
+        return cashValue * 0.0035;
+    }
+}
--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -1,6 +1,9 @@
-import org.junit.jupiter.api.Test;
+package rewardsconverter;
 
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 
 public class RewardValueTests {
 
